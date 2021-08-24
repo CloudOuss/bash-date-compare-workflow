4da136dd55e96543b8474dcc583664f3eab56e24
Oussama Souayeh <oussama@souayeh.pro>
2021-08-24 00:02:48 -0400

    Update script.sh

--- scripts/script.sh
+++ scripts/script.sh
@@ -2,7 +2,7 @@ echo "input date: $1"
 echo "current date: `date`"
 
 currentDate=`date`
-inputDate=$(date --date=${{ github.event.inputs.inputDate }} +%s)
+inputDate=$(date --date='2000-01-01' +%s)
 
 echo $currentDate
 echo $inputDate

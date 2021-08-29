9a3fdce4c63e89c6f55a46644653158a4cff1acc
Oussama Souayeh <oussama@souayeh.pro>
2021-08-29 14:17:38 -0400

    Update script.sh

--- scripts/script.sh
+++ scripts/script.sh
@@ -3,9 +3,11 @@
 currentDate=date +%s
 inputDate=$(date --date=$1 +%s)
 
-if [ $inputDate -ge $currentDate ]; #put the loop where you need it
-then
- echo "licenceExpired=false" >> $GITHUB_ENV
- else
-  echo "licenceExpired=true" >> $GITHUB_ENV
-fi
+#if [ $inputDate -ge $currentDate ]; #put the loop where you need it
+#then
+# echo "licenceExpired=[ $inputDate -ge $currentDate ]" >> $GITHUB_ENV
+# else
+#  echo "licenceExpired=true" >> $GITHUB_ENV
+#fi
+
+echo "licenceExpired=[ $inputDate -ge $currentDate ]" >> $GITHUB_ENV

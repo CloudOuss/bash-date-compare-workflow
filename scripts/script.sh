108fb2c0b6fd1d5dd77261cab482229edc6e0ea8
Oussama Souayeh <oussama@souayeh.pro>
2021-09-19 13:20:19 -0400

    Update script.sh

--- scripts/script.sh
+++ scripts/script.sh
@@ -1,10 +1,12 @@
-currentDate=$(date +'%s')
-inputDate=$(date --date=$1 +'%s')
-pendingDate=$(date +'%s' -d "$inputDate+$2 days")
+currentDateUnix=$(date +'%s')
+inputDate=$(date --date=$1)
+inputDateUnix=$(date --date=$1 +'%s')
+pendingDateUnix=$(date +'%s' -d "$inputDate+$2 days")
 
-echo $currentDate
-echo $pendingDate
-echo $inputDate
+echo "inputs $1 & $2"
+echo $currentDateUnix
+echo $pendingDateUnix
+echo $inputDateUnix
 
 if [ $inputDate -gt $currentDate ]; then
   echo "licenceStatus=valid" >> $GITHUB_ENV
